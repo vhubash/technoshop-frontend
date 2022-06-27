e37e74e2313f81dea39fd80cf006ea3af8ebcee4
Volodymyr Hubash <vhubash@gmail.com>
2022-06-27 03:48:48 +0300

    fix bugs

---
+++ src/screens/ProductScreen.js
@@ -0,0 +1,163 @@
+import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Button, Card, Form } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import { listProductDetails, createProductReview } from '../actions/productActions'
+import { PRODUCT_CREATE_REVIEW_RESET } from '../constants/productConstants'
+
+function ProductScreen({ match, history }) {
+    const [qty, setQty] = useState(1)
+    const [rating, setRating] = useState(0)
+    const [comment, setComment] = useState('')
+
+    const dispatch = useDispatch()
+
+    const productDetails = useSelector(state => state.productDetails)
+    const { loading, error, product } = productDetails
+
+    const userLogin = useSelector(state => state.userLogin)
+    const { userInfo } = userLogin
+
+    const productReviewCreate = useSelector(state => state.productReviewCreate)
+    const {
+        loading: loadingProductReview,
+        error: errorProductReview,
+        success: successProductReview,
+    } = productReviewCreate
+
+    useEffect(() => {
+        if (successProductReview) {
+            setRating(0)
+            setComment('')
+            dispatch({ type: PRODUCT_CREATE_REVIEW_RESET })
+        }
+
+        dispatch(listProductDetails(match.params.id))
+
+    }, [dispatch, match, successProductReview])
+
+    const addToCartHandler = () => {
+        history.push(`/cart/${match.params.id}?qty=${qty}`)
+    }
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        dispatch(createProductReview(
+            match.params.id, {
+            rating,
+            comment
+        }
+        ))
+    }
+
+    return (
+        <div>
+            <Link to='/' className='btn btn-light my-3'>Назад</Link>
+            {loading ?
+                <Loader />
+                : error
+                    ? <Message variant='danger'>{error}</Message>
+                    : (
+                        <div>
+                            <Row>
+                                <Col md={6}>
+                                    <Image src={product.image} alt={product.name} fluid />
+                                </Col>
+
+
+                                <Col md={3}>
+                                    <ListGroup variant="flush">
+                                        <ListGroup.Item>
+                                            <h3>{product.name}</h3>
+                                        </ListGroup.Item>
+
+                                        <ListGroup.Item>
+                                            <Rating value={product.rating} text={`${product.numReviews} відгуків`} color={'#f8e825'} />
+                                        </ListGroup.Item>
+
+                                        <ListGroup.Item>
+                                            Ціна: ₴{product.price}
+                                        </ListGroup.Item>
+
+                                        <ListGroup.Item>
+                                            Опис: {product.description}
+                                        </ListGroup.Item>
+                                    </ListGroup>
+                                </Col>
+
+
+                                <Col md={3}>
+                                    <Card>
+                                        <ListGroup variant='flush'>
+                                            <ListGroup.Item>
+                                                <Row>
+                                                    <Col>Ціна:</Col>
+                                                    <Col>
+                                                        <strong>${product.price}</strong>
+                                                    </Col>
+                                                </Row>
+                                            </ListGroup.Item>
+                                            <ListGroup.Item>
+                                                <Row>
+                                                    <Col>Статус:</Col>
+                                                    <Col>
+                                                        {product.countInStock > 0 ? 'В наявності' : 'Немає в наявності'}
+                                                    </Col>
+                                                </Row>
+                                            </ListGroup.Item>
+
+                                            {product.countInStock > 0 && (
+                                                <ListGroup.Item>
+                                                    <Row>
+                                                        <Col>Кількість</Col>
+                                                        <Col xs='auto' className='my-1'>
+                                                            <Form.Control
+                                                                as="select"
+                                                                value={qty}
+                                                                onChange={(e) => setQty(e.target.value)}
+                                                            >
+                                                                {
+
+                                                                    [...Array(product.countInStock).keys()].map((x) => (
+                                                                        <option key={x + 1} value={x + 1}>
+                                                                            {x + 1}
+                                                                        </option>
+                                                                    ))
+                                                                }
+
+                                                            </Form.Control>
+                                                        </Col>
+                                                    </Row>
+                                                </ListGroup.Item>
+                                            )}
+
+
+                                            <ListGroup.Item>
+                                                <Button
+                                                    onClick={addToCartHandler}
+                                                    className='btn-block'
+                                                    disabled={product.countInStock == 0}
+                                                    type='button'>
+                                                    Додати в кошик
+                                                </Button>
+                                            </ListGroup.Item>
+                                        </ListGroup>
+                                    </Card>
+                                </Col>
+                            </Row>
+
+                            
+                        </div>
+                    )
+
+            }
+
+
+        </div >
+    )
+}
+
+export default ProductScreen
